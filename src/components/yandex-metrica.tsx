fa2ca03a1655c01e9c82c844833b8ad46c831f46
ymatuhin <ym@ymatuhin.ru>
2023-08-27 03:40:45 +0300

    Lines moved to astro

---
+++ src/components/yandex-metrica.tsx
@@ -0,0 +1,7 @@
+// <!-- Yandex.Metrika counter -->
+// <img
+//   src="https://mc.yandex.ru/watch/42239694"
+//   style="position: absolute; left: -9999px"
+//   alt=""
+// />
+// <!-- /Yandex.Metrika counter -->
